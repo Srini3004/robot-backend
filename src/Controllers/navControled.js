0212db2da9df1a3b85a7bcd8085434122662fb68
Srini3004 <srinivasanbalakrishan04@gmail.com>
2024-08-09 15:20:23 +0530

    Nav control Update

--- src/Controllers/navControled.js
+++ src/Controllers/navControled.js
@@ -1,6 +1,79 @@
 
 
 
+// import History from "../Models/History.js";
+
+// const getRobots = async (req, res) => {
+//   try {
+//     const robots = await History.find();
+//     res.status(200).json(robots);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+// const createRobot = async (req, res) => {
+//   const {
+//     robotName,
+//     mapName,
+//     image,
+//     timeTaken,
+//     percentCompleted,
+//     status,
+//     linear_velocity,
+//     angular_velocity,
+//     position,
+//     orientation,
+//   } = req.body;
+
+// console.log("request bodys :",req.body)
+//   if (
+//     !robotName ||
+//     !mapName ||
+//     !image ||
+//     !timeTaken ||
+//     !percentCompleted ||
+//     !status ||
+//     !linear_velocity ||
+//     !angular_velocity ||
+//     !position ||
+//     !orientation
+//   ) {
+//     return res.status(400).json({ message: 'All fields are required' });
+//   }
+
+  
+//   const newHistory = new History({
+//     robotName,
+//     mapName,
+//     image,
+//     timeTaken,
+//     percentCompleted,
+//     status,
+//     linear_velocity,
+//     angular_velocity,
+//     position,
+//     orientation,
+//   });
+// console.log("new history :",newHistory)
+//   try {
+    
+//     const savedHistory = await newHistory.save();
+//     console.log("savehistory :",savedHistory)
+//     res.status(201).json(savedHistory);
+//   } catch (error) {
+//     res.status(400).json({ message: error.message });
+//   }
+// };
+
+
+ 
+
+// export { getRobots, createRobot };
+
+
+
+
 import History from "../Models/History.js";
 
 const getRobots = async (req, res) => {
@@ -26,13 +99,10 @@ const createRobot = async (req, res) => {
     orientation,
   } = req.body;
 
-console.log("request bodys :",req.body)
   if (
     !robotName ||
     !mapName ||
     !image ||
-    !timeTaken ||
-    !percentCompleted ||
     !status ||
     !linear_velocity ||
     !angular_velocity ||
@@ -42,31 +112,25 @@ console.log("request bodys :",req.body)
     return res.status(400).json({ message: 'All fields are required' });
   }
 
-  
   const newHistory = new History({
     robotName,
     mapName,
     image,
-    timeTaken,
-    percentCompleted,
+    timeTaken: timeTaken || 0,
+    percentCompleted: percentCompleted || 0,
     status,
     linear_velocity,
     angular_velocity,
     position,
     orientation,
   });
-console.log("new history :",newHistory)
+
   try {
-    
     const savedHistory = await newHistory.save();
-    console.log("savehistory :",savedHistory)
     res.status(201).json(savedHistory);
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
 };
-
-
- 
 
 export { getRobots, createRobot };
